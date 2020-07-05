c23034adb1875edb3b182c6183508704da4a3500
atul <38523309+atul2w@users.noreply.github.com>
2020-07-05 22:55:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	MONGODB:
-		"mongodb+srv://atul:ytrewq7745@cluster0-ubsco.mongodb.net/PROJECT0?retryWrites=true&w=majority",
+		"mongodb+srv://user:password@cluster0-ubsco.mongodb.net/PROJECT0?retryWrites=true&w=majority",
 	SECRET: "asdfghjkmnbvcxqwerty",
 };
